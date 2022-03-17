eeaef17b5ab93cd1985d60582ffa130fc5e1426b
51p50x <51plog50@gmail.com>
2022-03-16 20:59:21 -0600

    forms-6: created controlled component for select tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 
 const App = () =>{
-    const [value, setValue] = useState({normal:'init',texto:'por defecto'})
+    const [value, setValue] = useState({normal:'init',texto:'por defecto',select:''})
     const handleChange = (e) =>{
         setValue((state)=>({
             ...state,
@@ -14,6 +14,13 @@ const App = () =>{
             {value.length < 5 ? <span>longitud minima de 5</span> : null}
             <input type='text' name='normal' value={value.normal} onChange={handleChange}/>
             <textarea name='texto' onChange={handleChange} value={value.texto}/>
+            <select value={value.select} name="select" onChange={handleChange}>
+                <option value=''>--Seleccione--</option>
+                <option value='happywaifu'>--Happy Waifu--</option>
+                <option value='sadwaifu'>--Sad Waifu--</option>
+                <option value='angrywaifu'>--Angry Waifu--</option>
+                <option value='feelsbadman'>--No Waifu--</option>
+            </select>
         </div>
     )
 }
