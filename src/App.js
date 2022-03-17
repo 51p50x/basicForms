824f8b355a6c20650dc455b8cb4bb910e8d0e6bc
51p50x <51plog50@gmail.com>
2022-03-16 18:39:23 -0600

    forms-2: Created an alternative to access dom data with useRef hook, and an explanation on how I can work with file inputs. Also at the end is a fetch call just to validate that I can send binary data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import {useRef} from 'react'
 
-function App() {
-  const submit = (e) =>{
-    e.preventDefault();
-    const data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(data))
-  }
-  return (
-        <form onSubmit={submit}>
-          <div>
-            <span>
-              jalter
-            </span>
-            <input name='campo'/>
-          </div>
-          <input name='campo-2'/>
-          <input type='submit' value='Enviar'/>
-        </form>
-  )
+const App = () => {
+    const input = useRef()
+    const file = useRef()
+    const submit = () => {
+        console.log(input.current.value)
+        console.log(file.current.files[0])
+        const form = new FormData()
+        form.append('archivo',file.current.files[0])
+        form.append('campo',input.current.value)
+        fetch('/jalter',{method:'POST', body:form})
+    }
+    return(
+        <div>
+            <div>
+                <span>jalter</span>
+                <input type='text' name='campo' ref={input}/>
+                <input type='file' ref={file}/>
+            </div>
+            <input type='submit' value='enviar' onClick={submit}/>
+        </div>
+    )
 }
 
 export default App
