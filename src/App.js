2d08e03fa64907db83f077eb4ce68b5a407498c4
51p50x <51plog50@gmail.com>
2022-03-16 19:32:16 -0600

    forms-4: started with controlled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import {useState} from 'react'
 
-function App() {
-    const submit = (e) =>{
-      e.preventDefault();
-      const data = Array.from(new FormData(e.target))
-      console.log(Object.fromEntries(data))
+const App = () =>{
+    const [value, setValue] = useState('')
+    const handleChange = (e) =>{
+        setValue(e.target.value)
     }
-    return (
-          <form onSubmit={submit}>
-            <div>
-              <span>
-                jalter
-              </span>
-              <input name='campo'/>
-            </div>
-            <input name='campo-2'/>
-            <input type='file' name='archivo'/>
-            <input type='submit' value='Enviar'/>
-          </form>
+    console.log(value)
+    return(
+        <div>
+            {value.length < 5 ? <span>longitud minima de 5</span> : null}
+            <input type='text' name='normal' value={value} onChange={handleChange}/>
+        </div>
     )
-  }
-  
-  export default App
-  
+}
+
+
+export default App
