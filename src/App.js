1cf98891924e438db8c218491f93ded5b29d194d
51p50x <51plog50@gmail.com>
2022-03-16 19:05:04 -0600

    forms-3: created uncontrolled component that doesnt use useRef for file input types

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import {useRef} from 'react'
 
-const App = () => {
-    const input = useRef()
-    const file = useRef()
-    const submit = () => {
-        console.log(input.current.value)
-        console.log(file.current.files[0])
-        const form = new FormData()
-        form.append('archivo',file.current.files[0])
-        form.append('campo',input.current.value)
-        fetch('/jalter',{method:'POST', body:form})
+function App() {
+    const submit = (e) =>{
+      e.preventDefault();
+      const data = Array.from(new FormData(e.target))
+      console.log(Object.fromEntries(data))
     }
-    return(
-        <div>
+    return (
+          <form onSubmit={submit}>
             <div>
-                <span>jalter</span>
-                <input type='text' name='campo' ref={input}/>
-                <input type='file' ref={file}/>
+              <span>
+                jalter
+              </span>
+              <input name='campo'/>
             </div>
-            <input type='submit' value='enviar' onClick={submit}/>
-        </div>
+            <input name='campo-2'/>
+            <input type='file' name='archivo'/>
+            <input type='submit' value='Enviar'/>
+          </form>
     )
-}
-
-export default App
+  }
+  
+  export default App
+  
